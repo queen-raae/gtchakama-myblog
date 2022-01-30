4c81502cd1e4da125365bb0ef54930891979821c
George_Chakama <gtchakama@gmail.com>
2022-01-30 08:28:29 +0200

    created contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,51 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import {  Seo } from "@/components/common"
+import Layout from "@/components/Layout"
+import { PageProps } from "@/definitions"
+
+const BlogIndex: React.FC<PageProps> = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+ 
+
+ 
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo title="All posts" />
+       {/* <Bio /> */}
+      <h1>
+      Contact
+      </h1>
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+            tags
+          }
+        }
+      }
+    }
+  }
+`
