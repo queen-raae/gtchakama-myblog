49e1c171742857ef76e755abf921042f181aed09
George_Chakama <gtchakama@gmail.com>
2022-01-31 15:32:09 +0200

    Update Profile.tsx

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import { Github, Linkedin, Twitter } from "../icons"
 import { Button } from "."
@@ -29,21 +29,22 @@ const Bio: React.FC<EmptyProps> = () => {
 
   return (
     <>
-      <div className="flex items-center space-x-3">
+      <div className="">
         <StaticImage
           className="rounded-md overflow-hidden"
-          layout="fixed"
+          layout="fullWidth"
           formats={["auto", "webp", "avif"]}
-          src="https://i.ibb.co/G0zK7BW/ignite-23-01.jpg"
-          width={50}
-          height={50}
+          src="../../images/me.png"
+          // width={370}
+          // height={200}
           quality={95}
-          imgStyle={{ borderRadius: "20%" }}
+          imgStyle={{ borderRadius: "5px"}}
           alt="Profile picture"
         />
         <div>
-          <h1 className="font-bold font-exo text-2xl  tracking-wide">{author.name}</h1>
-          <div className="pt-2 flex space-x-4">
+          <h1 className="font-bold font-exo text-2xl mt-4 text-skin-para-green tracking-wide">{author.name}</h1>
+          <h2 className="text-2xl">Full-Stack Developer</h2>
+          <div className="pt-2 flex space-x-4 justify-start">
             <Button
               label="Twitter"
               href={`https://twitter.com/${social.twitter || ""}`}
@@ -62,15 +63,23 @@ const Bio: React.FC<EmptyProps> = () => {
             >
               <Github className="w-5 h-5 fill-current" />
             </Button>
+            <Button
+            label="Github"
+            href={`https://github.com/${social.github || ""}`}
+          >
+            <Github className="w-5 h-5 fill-current" />
+          </Button>
           </div>
         </div>
       </div>
       <p className="mt-5 font-yrsa text-lg text-skin-fg">I'm super passionate about exploring and building open-source web apps
           and tools. Currently, I'm diving into the React ecosystem with backend
           in Node.js</p>
-          <h1 className="font-bold font-exo text-2xl  tracking-wide">Assume Cool Feature.</h1>
-          <h1 className="font-bold font-exo text-2xl  tracking-wide">Mini Form.</h1>
-          <h1 className="font-bold font-exo text-2xl  tracking-wide">Copywrites and Year.</h1>
+          <div className="pt-2 flex space-x-4 justify-between">
+            <Link className="border rounded-md text-center hover:text-white p-1 w-1/3 hover:rounded-lg border-green-500 border-2 hover:bg-green-500 focus:ring-green-100 text-skin-fg" to="/">Read More</Link>
+            <Link className="border rounded-md text-center hover:text-white p-1 w-1/3 hover:rounded-lg border-green-500 border-2 hover:bg-green-500 focus:ring-green-100 text-skin-fg" to="/">Projects</Link>
+            <Link className="border rounded-md text-center hover:text-white p-1 w-1/3 hover:rounded-lg border-green-500 border-2 hover:bg-green-500 focus:ring-green-100 text-skin-fg" to="/">Hire Me</Link>
+          </div>
     </>
   )
 }
