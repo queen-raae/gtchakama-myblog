d60943e4f0feadae72aa4998b430e40aeadad11f
George_Chakama <gtchakama@gmail.com>
2022-02-07 16:24:14 +0200

    Create twentyone.ts

---
+++ src/components/timeline/twentyone.ts
@@ -0,0 +1,39 @@
+const twentyone = [{
+    date: "Oct 13 2020",
+    title:"Fell in Love with Gatsby JS",
+    description: "  Est aspernatur quae, eos explicabo odit minima libero veniam similique quibusdam oloribus facilis ipsa accusantium vel maiores corrupti! Libero voluptate a doloribus ",
+    link: "http://www.chakama.co.zw"},
+    
+    {
+    date: "Oct 13 2022",
+    title:"Hosted a Twiter Space",
+    description: "  Quibusdam oloribus facilis ipsa accusantium vel maiores corrupti! Libero voluptate a doloribus ",
+    link: "http://www.chakama.co.zw"},
+    {
+        date: "Oct 13 2022",
+        title:"Developed 10 Free NGO Sites",
+        description: "  Quibusdam oloribus facilis ipsa accusantium vel maiores corrupti! Libero voluptate a doloribus ",
+        link: "http://www.chakama.co.zw"},
+        {
+            date: "Oct 13 2022",
+            title:"Hosted a Twiter Space",
+            description: "  Quibusdam oloribus facilis ipsa accusantium vel maiores corrupti! Libero voluptate a doloribus ",
+            link: "http://www.chakama.co.zw"},
+            {
+                date: "Oct 13 2022",
+                title:"Started Blogging",
+                description: "  Quibusdam oloribus facilis ipsa accusantium vel maiores corrupti! Libero voluptate a doloribus ",
+                link: "http://www.chakama.co.zw"},
+            
+                {
+                    date: "Oct 13 2022",
+                    title:"Joined WeThinkCode as a Mentor",
+                    description: "  Quibusdam oloribus facilis ipsa accusantium vel maiores corrupti! Libero voluptate a doloribus ",
+                    link: "http://www.chakama.co.zw"},
+                    {
+                        date: "Oct 13 2022",
+                        title:"My article featured on Scrimba Twitter Space",
+                        description: "  Quibusdam oloribus facilis ipsa accusantium vel maiores corrupti! Libero voluptate a doloribus ",
+                        link: "http://www.chakama.co.zw"},]
+
+export default twentyone;
